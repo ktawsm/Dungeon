af138e3965511b7760423109b7ef806ec8338b47
Katy <kt.awsm29@gmail.com>
2025-10-06 04:24:01 -0500

    Implementing functions for load/save of player data

--- bot.py
+++ bot.py
@@ -2,19 +2,16 @@ import json
 import os
 import discord
 import random
+import enum
 from random import randint
 from discord.ext import commands
 from discord.commands import Option
 from dotenv import load_dotenv
 
-
-#########
 # CONNECTS GUILD ID AND BOT TOKEN THROUGH ENV VARIABLES
-#########
 load_dotenv()
 GUILD = str(os.getenv('DUNGEON_GUILD'))
 bot = commands.Bot(intents=discord.Intents.all(), command_prefix=str(os.getenv('DUNGEON_PREFIX')))
-
 
 #########
 # LISTS
@@ -22,11 +19,12 @@ bot = commands.Bot(intents=discord.Intents.all(), command_prefix=str(os.getenv('
 #########
 MOB_TYPE = ["Ogre", "Slime", "Skeleton", "Goblin", "Zombie", "Bandit"]
 
+
 #########
 # BASE CLASSES
 #########
 #########
-class item:
+class Item:
    def __init__(self):
        self.cost = 0
        self.hands = 0
@@ -36,13 +34,6 @@ class item:
        self.protection = 0
        self.durability = 1  
 
-class player:
-   def __init__(self):
-       self.health
-       self.head
-       self.hands = 2
-       self.extra_hand
-
 class Mob:
    def __init__(self, lvl: int):
        self.level = lvl
@@ -51,23 +42,56 @@ class Mob:
        self.damage = randint(5, 15)
        self.protection = randint(5, 10)
 
+class Player:
+   def __init__(self, user: discord.Member):
+        data = loadPlayerData(user)
+        self.level = data["player"]["level"]
+        self.experience
+        self.equipped = []
+
+#########
+# FUNCTIONS
+#########
+#########
+
+# Load player
+def loadPlayerData(player: discord.Member):
+    if os.path.exists(f"{player.id}.json"):
+        print("Loading player data: " + str({player.id}))
+        with open(f"{player.id}.json", 'r') as f:
+            playerDictionary = json.load(f)
+    else:
+        print("Loading player data as template: " + str({player.id}))
+        with open(f"UserTemplate.json", 'r') as f:
+            playerDictionary = json.load(f)
+    return playerDictionary
+
+# Save player
+def savePlayerData(player:discord.Member, dictionary: dict):
+    print("Saving player data: " + str({player.id}))
+    with open(f"Users\\{player.id}.json", 'w') as f:
+        f = json.dump(dictionary, f, indent=2)
+
+
 #########
 # GAME
 #########
 #########
+
+# RAID COMMAND 
 @bot.slash_command(guild_ids=[GUILD], name="raid", description="Enter the dungeon and fight monstuhs")
 async def raid(ctx):
-    pass
-
-    ## INITIATES PLAYER DATA BY LOADING THEIR EXISTING ASSOCIATED JSON FILE, OR IF ONE ISNT FOUND LOADS THE USER TEMPLATE AS THEIR DATA
-    if os.path.exists(f"{ctx.author.id}.json"):
-        with open(f"{ctx.author.id}.json", "r") as f: # r = read mode, w = write mode
-            player = json.load(f) # data is now a dict
-        print("--- AUTHOR USER DATA LOADED ---")
-    else:
-        with open("UserTemplate.json", "r") as f: # r = read mode, w = write mode
-            player = json.load(f) # data is now a dict
-        print("--- USER TEMPLATE DATA LOADED FOR AUTHOR ---")
+  #  ## (remaking as its own function) INITIATES PLAYER DATA BY LOADING THEIR EXISTING ASSOCIATED JSON FILE, OR IF ONE ISNT FOUND LOADS THE USER TEMPLATE AS THEIR DATA
+  #  if os.path.exists(f"{ctx.author.id}.json"):
+  #      with open(f"{ctx.author.id}.json", "rw") as f: # r = read mode, w = write mode
+  #          player = json.load(f) # data is now a dict
+  #          json.dump(player, f, indent = 4)
+  #      print("--- AUTHOR USER DATA LOADED ---")
+  #  else:
+  #      with open("UserTemplate.json", "r") as f: # r = read mode, w = write mode
+  #          player = json.load(f) # data is now a dict
+  #      print("--- USER TEMPLATE DATA LOADED FOR AUTHOR ---")
+    playerData = loadPlayerData(ctx.author)
 
     ## GENERATES ENEMY TEAM
     enemyTeam = []
@@ -77,8 +101,10 @@ async def raid(ctx):
         mob = Mob(1)
         enemyTeam.append(mob)
         EncounterText += f"{mob.type}: {mob.health}hp\n"
-
+    print(f"Generated player data: " + str(EncounterText))
+    savePlayerData(ctx.author, playerData)
     await ctx.respond(EncounterText)
+
 
 #########
 # RUN
